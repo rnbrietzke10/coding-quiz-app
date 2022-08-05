15ae31c428babecbae3bb863fd1f2744a8e2b828
Rebecca Brietzke <rnbrietzke@outlook.com>
2022-08-05 11:59:50 -0500

    working on quiz layout with question and answer choices

--- seed.py
+++ seed.py
@@ -1,8 +1,7 @@
 from app import app
 from models import db, User
 
-
-db.drop_all()
+# db.drop_all()
 db.create_all()
 
 """
@@ -15,14 +14,12 @@ db.create_all()
     image_url = db.Column(db.Text,default="./static/assets/blank-profile-picture-973460.svg",)
 """
 
-
 u1 = User.sign_up(
     first_name="Sharon",
     last_name="Stone",
     email="sharon@gmail.com",
     username="sharon123",
     password="apples",
-    image_url="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
 )
 
 u2 = User.sign_up(
@@ -31,7 +28,6 @@ u2 = User.sign_up(
     email="ryan@gmail.com",
     username="ryan123",
     password="apples",
-    image_url="https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
 )
 
 db.session.commit()
